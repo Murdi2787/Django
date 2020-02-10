b7f0b7c31242bbf4bb0f00162a8631804911b934
Murdi2787 <aleks.shavlack@gmail.com>
2020-02-10 21:46:59 +0200

    withoutfront

--- my_shop/cart/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-
-PRODUCT_AMOUNT_CHOICES = [(i, str(i)for i in range(1, 21))]
-
-
-class CartAddProductForm(forms.Form):
-    amount = forms.TypedChoiceField(choices=PRODUCT_AMOUNT_CHOICES, coerce=int)
-    update = forms.BooleanField(required=False, initial=False, widget=forms.HiddenInput)
